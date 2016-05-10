ac3164460150ea0d10bd80a68a81d9c6ae7d9bd7
jacmarjorie <jacmarjorie@gmail.com>
2016-05-10 10:13:08 -0700

    fix

--- test/test_bcftool_static.py
+++ test/test_bcftool_static.py
@@ -6,4 +6,4 @@ class TestBCF2():
   def test_bcf_static(self):
 
     out1 = pysam.bcftools.norm("-m", "+any", "-O", "z", "-o", "test/data/test1.sorted.vcf.gz", "test/data/test1.vcf", catch_stdout=False)
-    assert out1 == ''
+    assert out1 == None
