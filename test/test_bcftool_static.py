ef2365afa62cb2d249b85c2e42e4deab52568f90
jacmarjorie <jacmarjorie@gmail.com>
2016-05-10 10:12:16 -0700

    fix

--- test/test_bcftool_static.py
+++ test/test_bcftool_static.py
@@ -5,6 +5,5 @@ class TestBCF2():
   
   def test_bcf_static(self):
 
-    out1 = pysam.bcftools.norm("-m", "+any", "-O", "z", "-o", "test/data/test1.sorted.vcf.gz", "test/data/test1.vcf")
-  # out2 = bcftools.index(sorted_file, "-t", "-f")
+    out1 = pysam.bcftools.norm("-m", "+any", "-O", "z", "-o", "test/data/test1.sorted.vcf.gz", "test/data/test1.vcf", catch_stdout=False)
     assert out1 == ''
